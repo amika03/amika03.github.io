632aae799d9ac62f17a6ad89031b681c412d2f79
amika03 <amiaob03@gmail.com>
2024-09-25 21:04:48 +0700

    mika

---
+++ RESU.js
@@ -0,0 +1,21 @@
+function greet() {
+    alert("Welcom in Resume");
+}
+function ady() {
+    var image = document.getElementById("hiddenady");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }
+}
+
+function pea() {
+    var image = document.getElementById("hiddenpea");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }
+}
+
